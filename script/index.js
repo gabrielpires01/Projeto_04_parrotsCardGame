d75bafe05bddeefde55c9890fdc80193b7aff266
Gabriel Pires <gabriel.pires@twistsystems.com>
2021-07-31 23:44:59 -0300

    Fixed bugs and refactored

--- script/index.js
+++ script/index.js
@@ -7,6 +7,7 @@ let seconds = 0;
 let pair = [];
 let done = 0;
 let stoptime = false;
+let id;
 
 function getNumCards () {
     numCards = Number(prompt("Quantas cartas quer jogar?"))
@@ -18,15 +19,9 @@ function getNumCards () {
 
 
 function inicializer() {
-    done = 0;
-    seconds = 0;
-    clicked = 0;
-    plays = 0;
-    stoptime = false;
-
     numCards = getNumCards()
 
-    const element = document.querySelector('.cards');
+    const element = document.querySelector('main');
     images.sort(comparador);
 
     const imagens = images.slice(0, numCards/2);
@@ -63,18 +58,17 @@ function inicializer() {
 
 function choose(e) {
     if (clicked < 2) {
-        e.querySelector('.back').style.transform = 'rotateY(0deg)'
-        e.querySelector('.front').style.transform = 'rotateY(-180deg)'
+        rotateCard(e,'toBack')
         pair.push(e)
         clicked ++
         plays ++
         if (clicked === 2){
+            // compares source values
             if (pair[0].querySelector('.back img').attributes.src.value !== pair[1].querySelector('.back img').attributes.src.value) {
                 setTimeout(function(){
-                    pair[0].querySelector('.back').style.transform = 'rotateY(-180deg)'
-                    pair[0].querySelector('.front').style.transform = 'rotateY(0deg)'
-                    pair[1].querySelector('.back').style.transform = 'rotateY(-180deg)'
-                    pair[1].querySelector('.front').style.transform = 'rotateY(0deg)'
+                    // used Index selections, was too short for a loop
+                    rotateCard(pair[0],'toFront')
+                    rotateCard(pair[1],'toFront')
                     clicked = 0
                     pair = []
                 }, 1000);
@@ -91,16 +85,38 @@ function choose(e) {
     }
 }
 
+const rotateCard = (e, rotation) => {
+    if (rotation === 'toBack') {
+        e.querySelector('.back').style.transform = 'rotateY(0deg)'
+        e.querySelector('.front').style.transform = 'rotateY(-180deg)'
+    } else {
+        e.querySelector('.back').style.transform = 'rotateY(-180deg)'
+        e.querySelector('.front').style.transform = 'rotateY(0deg)'
+    }
+
+}
+
 function timer(){
     if (stoptime === false) {
-        document.getElementById('safeTimerDisplay').innerHTML = seconds + 's';
-        seconds ++
-        setTimeout(timer, 1000)
+        id = setInterval(() => {
+            document.getElementById('safeTimerDisplay').innerHTML = seconds + 's';
+            seconds++
+        }, 1000);
+    } else {
+        clearInterval(id)
     }
 }
 
 function restart () {
+    seconds = 0
+    done = 0;
+    clicked = 0;
+    plays = 0;
+    stoptime = false;
     document.querySelector('.cards').innerHTML = '';
+
+    document.getElementById('safeTimerDisplay').innerHTML = seconds + 's';
+    clearInterval(id)
     inicializer()
 }
 
